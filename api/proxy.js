c0f44c3d554fa1b9519f4815d6e3d6084832530d
杨鹏刚 <yangpenggang@yangpenggangdeMacBook-Pro.local>
2023-07-25 14:06:25 +0800

    aaaaaa

--- api/proxy.js
+++ api/proxy.js
@@ -8,7 +8,7 @@ import { createProxyMiddleware} from 'http-proxy-middleware'
     // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
     // target 替换为你跨域请求的服务器 如： http://gmall-h5-api.atguigu.cn
     if (req.url.startsWith('/api')) {
-        target = 'http://autumnfish.cn/'
+        target = 'http://autumnfish.cn'
     }
     // 创建代理对象并转发请求
     createProxyMiddleware({
@@ -17,7 +17,7 @@ import { createProxyMiddleware} from 'http-proxy-middleware'
         pathRewrite: {
             // 通过路径重写，去除请求路径中的 `/api`
             // 如果开启了,那么 /api/user/login 将被转发到 http://gmall-h5-api.atguigu.cn/user/login
-            '^/api/': '/',
+            '^/api': '',
         },
     })(req, res)
 }
